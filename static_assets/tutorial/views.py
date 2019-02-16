017fb0f315356a1ecc0631ae0346f8c6d1833b86
EugeneKalentev <EugeneKalentev@gmail.com>
2019-02-16 08:22:00 +0400

    Add delete link in WikiPagegit add --all!

---
+++ static_assets/tutorial/views.py
@@ -0,0 +1,21 @@
+from pyramid.response import Response
+from pyramid.view import (
+							view_config,
+							view_defaults
+						)
+
+# First view, available at http://localhost:6543/
+@view_defaults(renderer='home.pt')
+class TutorialViews:
+	def __init__(self, request):
+		self.request = request
+
+	@view_config(route_name='home')
+	def home(self):
+		return {'name': 'Home View'}
+
+# /howdy
+
+	@view_config(route_name='hello')	
+	def hello(self):
+		return {'name': 'Hello View'}
